58750ae47cc4a1c9ffe6577fd4dc2b7730219794
Timo Bechtel <48655057+TimoBechtel@users.noreply.github.com>
2026-05-05 13:08:13 +0200

    feat: add chromium browser engine

--- Sources/CmuxSettingsJSONPathSupport.swift
+++ Sources/CmuxSettingsJSONPathSupport.swift
@@ -453,6 +453,7 @@ extension CmuxSettingsFileStore {
         "browser.customSearchEngineName",
         "browser.customSearchEngineURLTemplate",
         "browser.showSearchSuggestions",
+        "browser.engine",
         "browser.theme",
         "browser.discardHiddenWebViews",
         "browser.hiddenWebViewDiscardDelaySeconds",
