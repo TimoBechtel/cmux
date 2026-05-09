cac16f3a013c2c41c008e1e54678baefaad777fd
Timo Bechtel <48655057+TimoBechtel@users.noreply.github.com>
2026-05-09 11:35:11 +0200

    fix: split view chromium focus

--- Sources/Panels/BrowserPanel.swift
+++ Sources/Panels/BrowserPanel.swift
@@ -7773,6 +7773,11 @@ extension BrowserPanel {
             return .browser(.webView)
         }
 
+        if let chromiumHostView,
+           Self.responderChainContains(responder, target: chromiumHostView) {
+            return .browser(.webView)
+        }
+
         return nil
     }
 
