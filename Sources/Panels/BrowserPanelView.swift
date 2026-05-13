53f08d9258e536923a1c7518fc0f5dff2a2370e1
Timo Bechtel <48655057+TimoBechtel@users.noreply.github.com>
2026-05-13 23:02:19 +0200

    fix(chromium): focus browser content on click

--- Sources/Panels/BrowserPanelView.swift
+++ Sources/Panels/BrowserPanelView.swift
@@ -899,8 +899,7 @@ struct BrowserPanelView: View {
     }
 
     private func handleBrowserWebViewClickIntent(_ notification: Notification) {
-        guard let webView = notification.object as? CmuxWebView,
-              webView === panel.webView else {
+        guard panel.browserFocusNotificationWindow(for: notification.object) != nil else {
             return
         }
 #if DEBUG
@@ -910,6 +909,7 @@ struct BrowserPanelView: View {
             "addressFocused=\(addressBarFocused ? 1 : 0)"
         )
 #endif
+        panel.noteWebViewFocused()
         if addressBarFocused {
 #if DEBUG
             logBrowserFocusState(event: "addressBarFocus.webViewClickBlur")
